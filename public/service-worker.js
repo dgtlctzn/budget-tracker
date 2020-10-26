d08ba8966710af6444ddf305481ad71818fed7c4
Joseph Perry <josephperry720@gmail.com>
2020-10-26 16:49:37 -0400

    added service worker file and install function

---
+++ public/service-worker.js
@@ -0,0 +1,27 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/transaction.js",
+    "favicon.ico",
+    "index.js",
+    "manifest.webmanifest",
+    "service-worker.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "styles.css"
+  ];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(staticFilesToPreCache);
+    })
+  );
+
+  self.skipWaiting();
+});
